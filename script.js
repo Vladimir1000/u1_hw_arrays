0426e8892df6838d89693ba022a129976e01e3dd
Vladimir1000 <Vladimir.dzelovski@gmail.com>
2024-04-30 19:30:40 -0500

    completed prompt 5

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ Exercise 5: Insert an element between two others
 Complete Exercise 5 in the space below:
 */
 
+foods.splice(2,0,'tofu');
 
-
-// console.log('Exercise 5 result:', foods);
+console.log('Exercise 5 result:', foods);
 
 
 /*
